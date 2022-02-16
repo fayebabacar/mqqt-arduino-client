cd541f628fde9a60d5da7e3946e03cd0a488a3e0
fayebabacar <mbayefaye118@gmail.com>
2022-02-16 18:17:55 +0000

    commit arduino with mqtt

---
+++ System.cpp
@@ -0,0 +1,40 @@
+/*
+* The MIT License (MIT)
+*
+* Copyright (C) 2018 Copyright (c) 2018 Gabriel Nikol
+*/
+
+#include "System.h"
+
+void System::set_heartbeat(uint32_t period) {
+    this->heartbeat_period = period;
+    this->heartbeat_current = millis();
+}
+
+uint32_t System::get_heartbeat() {
+    return this->heartbeat_period;
+}
+
+bool System::has_beaten() {
+    uint32_t current = millis();
+    if (current - heartbeat_current > heartbeat_period) {
+        this->heartbeat_current = current;
+        return true;
+    }
+    return false;
+}
+
+uint32_t System::get_elapsed_time() {
+    uint32_t current = millis();
+    uint32_t elapsed_time = current - elapsed_current;
+    elapsed_current = current;
+    return elapsed_time;
+}
+
+void System::sleep(uint32_t duration) {
+    delay(duration);
+}
+
+void System::exit() {
+    ESP.restart();
+}
---
+++ global_defines.h
@@ -0,0 +1,35 @@
+/*
+* The MIT License (MIT)
+*
+* Copyright (C) 2018 Copyright (c) 2018 Gabriel Nikol
+*/
+
+#ifndef ARDUINO_MQTTSN_CLIENT_GLOBAL_DEFINES_H
+#define ARDUINO_MQTTSN_CLIENT_GLOBAL_DEFINES_H
+
+#include <cstdint>
+
+struct device_address {
+    uint8_t bytes[6];  // mac
+    device_address() {
+        bytes[0] = 0x0;
+        bytes[1] = 0x0;
+        bytes[2] = 0x0;
+        bytes[3] = 0x0;
+        bytes[4] = 0x0;
+        bytes[5] = 0x0;
+    }
+};
+
+void printDeviceAddress(device_address *address) {
+    for (uint8_t i = 0; i < sizeof(device_address); i++) {
+        if (i == sizeof(device_address) - 1) {
+            Serial.print(address->bytes[i]);
+        } else {
+            Serial.print(address->bytes[i]);
+            Serial.print(", ");
+        }
+    }
+}
+
+#endif //ARDUINO_MQTTSN_CLIENT_GLOBAL_DEFINES_H
